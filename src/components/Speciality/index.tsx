b4c0d024f8c62b790c20ee5ed2e3fbcd9854c50b
lucasrs <lucasrs.rodrigues3@outlook.com>
2022-05-24 17:20:27 -0300

    start spec

--- src/components/Speciality/index.tsx
+++ src/components/Speciality/index.tsx
@@ -2,8 +2,43 @@ import styles from '@/styles/components/Speciality.module.scss';
 
 function Speciality() {
   return (
-    <section className={`${styles.products} section`}>
+    <section className={`${styles.spec} section container`}>
+      <div className="spec__container">
+        <div className="spec__box">
+          <h2 className="section__title">Speciality coffes that make you happy and cheer you up!</h2>
 
+          <div>
+            <a href="#" className="button spec__button">
+              See more
+            </a>
+          </div>
+        </div>
+
+        <div className="spec__category">
+          <div className="spec__group">
+            <img src="assets/img/specialty1.png" alt="" className="spec__img" />
+
+            <h3 className="spec__title"></h3>
+            <p className="spec__description"></p>
+          </div>
+
+          <div className="spec__group">
+            <img src="assets/img/specialty2.png" alt="" className="spec__img" />
+
+            <h3 className="spec__title"></h3>
+            <p className="spec__description"></p>
+          </div>
+
+          <div className="spec__group">
+            <img src="assets/img/specialty3.png" alt="" className="spec__img" />
+
+            <h3 className="spec__title"></h3>
+            <p className="spec__description"></p>
+          </div>
+        </div>
+
+
+      </div>
     </section>
   )
 }
